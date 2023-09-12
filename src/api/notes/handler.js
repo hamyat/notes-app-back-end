ed0595985b6ad0e73b10ce74c040ea3cb98afb00
Hamyat <hamyat86@gmail.com>
2023-09-12 13:04:35 +0700

    user registration implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,7 +1,5 @@
-/* eslint-disable no-console */
 const ClientError = require('../../exceptions/ClientError');
 
-/* eslint-disable no-underscore-dangle */
 class NotesHandler {
   constructor(service, validator) {
     this._service = service;
